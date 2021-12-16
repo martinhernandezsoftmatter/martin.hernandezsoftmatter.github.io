80213f520f6d9e1e1d7d1bfa623f2d4c1a4eda9d
André Michel Pozos <andreandyp@outlook.com>
2021-12-15 23:53:23 -0600

    Añadidos atributos ARIA y etiqueta meta de descripcion

--- js/navbar.js
+++ js/navbar.js
@@ -7,4 +7,10 @@ hamburguerButton.addEventListener("click", toggleNavbar);
 function toggleNavbar() {
 	hamburguerButton.classList.toggle("is-active");
 	document.querySelector("#navbar-menu").classList.toggle("is-active");
+	const currentExpandedAria = hamburguerButton.getAttribute("aria-expanded");
+	if(currentExpandedAria === "true") {
+		hamburguerButton.setAttribute("aria-expanded", "false");
+	} else {
+		hamburguerButton.setAttribute("aria-expanded", "true");
+	}
 }
